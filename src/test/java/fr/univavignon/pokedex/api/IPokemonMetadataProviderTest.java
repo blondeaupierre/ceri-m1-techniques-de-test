fe8dcce1c4fb0311d2459e705538269d2f3964c2
Pierre <niono235@gmail.com>
2024-02-13 08:39:04 +0100

    TP2 : Add unitary tests

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,45 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class IPokemonMetadataProviderTest {
+
+    @Mock
+    private static PokemonMetadata pokemonMetadata;
+
+    @BeforeAll
+    public static void setUp() {
+        pokemonMetadata = new PokemonMetadata(1, "Bulbasaur", 126, 126, 90);
+    }
+    @Test
+    public void testGetName() {
+        assertEquals(pokemonMetadata.getName(), "Bulbasaur");
+    }
+
+    @Test
+    public void testGetAttack() {
+        assertEquals(pokemonMetadata.getAttack(), 126);
+    }
+
+    @Test
+    public void testGetDefense() {
+        assertEquals(pokemonMetadata.getDefense(), 126);
+    }
+
+    @Test
+    public void testGetStamina() {
+        assertEquals(pokemonMetadata.getStamina(), 90);
+    }
+
+    @Test
+    public void testGetIndex() {
+        assertEquals(pokemonMetadata.getIndex(), 1);
+    }
+}
