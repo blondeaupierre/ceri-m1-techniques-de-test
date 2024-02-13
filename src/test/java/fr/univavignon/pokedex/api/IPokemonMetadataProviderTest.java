e8418816642fce372d7a0539694e45ac7bd3b7d4
Pierre <niono235@gmail.com>
2024-02-13 10:44:55 +0100

    TP3 : unit tests for IPokemonMetadataProviderTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,45 +1,49 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class IPokemonMetadataProviderTest {
 
     @Mock
-    private static PokemonMetadata pokemonMetadata;
+    private static IPokemonMetadataProvider pokemonMetadataProvider;
 
     @BeforeAll
-    public static void setUp() {
-        pokemonMetadata = new PokemonMetadata(1, "ulbasaur", 126, 126, 90);
+    public static void setUp() throws PokedexException {
+        pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+        when(pokemonMetadataProvider.getPokemonMetadata(1)).thenReturn(new PokemonMetadata(1, "Bulbasaur", 126, 126, 90));
+
     }
     @Test
-    public void testGetName() {
-        assertEquals(pokemonMetadata.getName(), "Bulbasaur");
+    public void testGetName() throws PokedexException {
+        assertEquals(pokemonMetadataProvider.getPokemonMetadata(1).getName(), "Bulbasaur");
+    }
+    @Test
+    public void testGetAttack() throws PokedexException {
+        assertEquals(pokemonMetadataProvider.getPokemonMetadata(1).getAttack(), 126);
     }
 
     @Test
-    public void testGetAttack() {
-        assertEquals(pokemonMetadata.getAttack(), 126);
+    public void testGetDefense() throws PokedexException {
+        assertEquals(pokemonMetadataProvider.getPokemonMetadata(1).getDefense(), 126);
     }
 
     @Test
-    public void testGetDefense() {
-        assertEquals(pokemonMetadata.getDefense(), 126);
+    public void testGetStamina() throws PokedexException {
+        assertEquals(pokemonMetadataProvider.getPokemonMetadata(1).getStamina(), 90);
     }
 
     @Test
-    public void testGetStamina() {
-        assertEquals(pokemonMetadata.getStamina(), 90);
-    }
-
-    @Test
-    public void testGetIndex() {
-        assertEquals(pokemonMetadata.getIndex(), 1);
+    public void testGetIndex() throws PokedexException {
+        assertEquals(pokemonMetadataProvider.getPokemonMetadata(1).getIndex(), 1);
     }
 }
