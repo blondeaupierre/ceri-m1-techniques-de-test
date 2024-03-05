2b39053d1f0fc5224a5e14170c93d2ec939de102
Pierre <niono235@gmail.com>
2024-03-05 09:16:32 +0100

    TP3 : unit tests IPokedexFactoryTest

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -0,0 +1,47 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class IPokemonTrainerFactoryTest {
+
+    @Mock
+    private static IPokemonTrainerFactory pokemonTrainerFactory;
+
+    @Mock
+    private static IPokedexFactory pokedexFactory;
+
+    @Mock
+    private static IPokedex pokedex;
+
+    @BeforeAll
+    public static void setUp() throws PokedexException {
+        pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
+        pokedexFactory = mock(IPokedexFactory.class);
+        pokedex = mock(IPokedex.class);
+        when(pokemonTrainerFactory.createTrainer("baka", Team.VALOR, pokedexFactory)).thenReturn(new PokemonTrainer("baka", Team.VALOR, pokedex));
+    }
+
+    @Test
+    public void testGetName() {
+        PokemonTrainer trainer = pokemonTrainerFactory.createTrainer("baka", Team.VALOR, pokedexFactory);
+        assertEquals("baka", trainer.getName());
+    }
+
+    @Test
+    public void testGetTeam() {
+        PokemonTrainer trainer = pokemonTrainerFactory.createTrainer("baka", Team.VALOR, pokedexFactory);
+        assertEquals(Team.VALOR, trainer.getTeam());
+    }
+
+    @Test
+    public void testGetPokedex() {
+        PokemonTrainer trainer = pokemonTrainerFactory.createTrainer("baka", Team.VALOR, pokedexFactory);
+        assertEquals(pokedex, trainer.getPokedex());
+    }
+}
