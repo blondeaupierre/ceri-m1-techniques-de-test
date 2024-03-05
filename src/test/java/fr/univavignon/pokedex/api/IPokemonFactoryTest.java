132cd95d9fd1cecfa7af6c24fdf20d9e4d71093e
Pierre <niono235@gmail.com>
2024-03-05 10:15:32 +0100

    TP3 : pokemonFactory test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -0,0 +1,27 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class IPokemonFactoryTest {
+
+    @Mock
+    private static IPokemonFactory pokemonFactory;
+
+    @BeforeAll
+    public static void setUp() {
+        pokemonFactory = mock(IPokemonFactory.class);
+        when(pokemonFactory.createPokemon(1, 613, 64, 4000, 4)).thenReturn(new Pokemon(1, "Bulbasaur", 613, 64, 4000, 4, 4000, 4, 4, 4));
+    }
+
+    @Test
+    public void testCreatePokemon() {
+        Pokemon pokemon = pokemonFactory.createPokemon(1, 613, 64, 4000, 4);
+        assertEquals(pokemonFactory.createPokemon(1, 613, 64, 4000, 4), pokemon);
+    }
+}
