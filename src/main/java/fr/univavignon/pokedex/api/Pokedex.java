84c56bc45316d8b6217f6519df09c207b79609dd
Pierre <niono235@gmail.com>
2024-03-16 18:29:50 +0100

    TP4 : AddPokemon now return len instead of len -1

--- src/main/java/fr/univavignon/pokedex/api/Pokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -19,7 +19,7 @@ public class Pokedex implements IPokedex {
     @Override
     public int addPokemon(Pokemon pokemon) {
         pokemons.add(pokemon);
-        return pokemons.size() - 1;
+        return pokemons.size();
     }
 
     @Override
