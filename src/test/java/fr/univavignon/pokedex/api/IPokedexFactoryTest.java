695bd19b33f6dc17a9f70c3b16535b855d86b2d9
Pierre <niono235@gmail.com>
2024-03-19 17:26:01 +0100

    TP4 : Implémentation création pokedex

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -4,13 +4,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class IPokedexFactoryTest {
 
-    @Mock
     private static IPokedexFactory pokedexFactory;
     @Mock
     private static IPokemonMetadataProvider pokemonMetadataProvider;
@@ -21,16 +20,15 @@ public class IPokedexFactoryTest {
 
     @BeforeAll
     public static void setUp() {
-        pokedexFactory = mock(IPokedexFactory.class);
         pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
         pokemonFactory = mock(IPokemonFactory.class);
-        pokedex = mock(IPokedex.class);
-        when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(pokedex);
+
+        pokedexFactory = new PokedexFactory();
     }
 
     @Test
     public void testCreatePokedex() {
         IPokedex pokedex = pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory);
-        assertEquals(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory), pokedex);
+        assertNotNull(pokedex);
     }
 }
